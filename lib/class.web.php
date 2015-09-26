e674e01584aff953f3ba722f23c28d93c79ad79b
David256 <davidborja456@gmail.com>
2015-09-26 10:38:53 -0500

    Se añade atributo y metodos a clase web

--- lib/class.web.php
+++ lib/class.web.php
@@ -1,8 +1,36 @@
 <?php 
 
 
-class Web(){
-	
+class Web
+{
+	private $id = '';
+	private $name = '';
+	private $charset = '';
+
+	function setId($newId){
+		$id = $newId;
+	}
+
+	function setName($newName){
+		$name = $newName;
+	}
+
+	function setCharset($newChar){
+		$charset = $newChar;
+	}
+
+
+	function getId(){
+		return $id;
+	}
+
+	function getName(){
+		return $name;
+	}
+
+	function getCharset(){
+		return $charset;
+	}
 }
 
 
