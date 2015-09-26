57086c915fe393c5995e95e93104d7f0822e9f93
David256 <davidborja456@gmail.com>
2015-09-26 11:06:20 -0500

    mejora en la clase style

--- lib/class.html.style.php
+++ lib/class.html.style.php
@@ -1,1 +1,55 @@
-s
+<?php 
+
+
+class Style
+{
+	private $media = '';
+	private $scoped = '';
+	private $type = '';
+	private $src = '';
+
+	function setMedia($newMedia){
+		$this->media = $newMedia;
+	}
+
+	function setScoped($newSco){
+		$this->scoped = $newSco;
+	}
+
+	function setType($newType){
+		$this->type = $newType;
+	}
+
+
+	private function setSrc($newSrc){
+		$this->src = $newSrc;
+	}
+
+
+	function getMedia(){
+		return $this->media;
+	}
+
+	function getScoped(){
+		return $this->scoped;
+	}
+
+	function getType(){
+		return $this->type;
+	}
+
+	function getSrc(){
+		return $this->src;
+	}
+
+
+	function loadFile($rutaString){
+		setSrc($rutaString);
+	}
+
+	function loadFile(File $file){
+		setSrc($file->getSrc());
+	}
+}
+
+ ?>
