790d8c3fc1317573c27325f9180344da49d53487
David256 <davidborja456@gmail.com>
2015-09-26 14:36:00 -0500

    Ejemplo archivo

--- test/simple.php
+++ test/simple.php
@@ -1,1 +1,27 @@
-s
+<?php 
+
+require('../lib/class.web.php');
+require('../lib/class.html.php');
+require('../lib/class.html.style.php');
+require('../lib/class.head.php');
+
+//creamos objetos web que es la página en si
+//creamos objeto html que contiene todo el codigo html
+$web = new Web();
+$html = new Html();
+$style = new Style();
+$head = new Head();
+
+$style->setMedia('display');
+$style->setType('text/css');
+$style->cargar('../css/ejemplo.css');
+
+$head->add($style);
+$html->add($head);
+
+
+
+$web->add($html);
+echo $web->showAll();
+
+ ?>
