f260ccd7aa2b71c8e7f418885723fe46557fc0e2
David256 <davidborja456@gmail.com>
2015-10-21 06:59:16 -0500

    Se agrego comentarios explicativos

--- test/simple.php
+++ test/simple.php
@@ -12,16 +12,23 @@ $html = new Html();
 $style = new Style();
 $head = new Head();
 
+//usamos el objeto style para configurarlo con un media
 $style->setMedia('display');
+//un tipo de estilo
 $style->setType('text/css');
+//y el pasamos la ruta donde se encuentra el estilo
 $style->cargar('../css/ejemplo.css');
 
+
+//ahora lo añadimos a head
 $head->add($style);
+//y este finalmente al documento html
 $html->add($head);
 
 
-
+//tenemos que anadir el documento html a el gran objeto web
 $web->add($html);
+//para luego ser mostrado con todas las cosas cargadas
 echo $web->showAll();
 
  ?>
